eb1b9189142374c7a27382e7a2369780b0755014
Marek <marek.gerwel@gmail.com>
2023-09-21 08:51:53 +0200

    initial commit

---
+++ tests/ScoreBoardTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ScoreBoard\Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversNothing
+ */
+class ScoreBoardTest extends TestCase
+{
+    public function testTeam(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testGame(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testBoard(): void
+    {
+        $this->assertTrue(true);
+    }
+}
